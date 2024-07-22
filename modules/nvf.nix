d2bb205b3be612594d000bfb201e1069bcebd0e0
Erwann Lagouche (AirOne01) <21955960+AirOne01@users.noreply.github.com>
2024-07-22 22:19:07 +0200

    feat(nvf): add some config and some lsp

--- modules/nvf.nix
+++ modules/nvf.nix
@@ -1,11 +1,19 @@
-{...}: {
+_: {
   programs.nvf = {
     enable = true;
 
     settings = {
       vim = {
-        #viAlias = true;
-        #vimAlias = true;
+        viAlias = true;
+        vimAlias = true;
+        lsp = {
+          enable = true;
+        };
+        languages = {
+          nix.enable = true;
+          html.enable = true;
+          ts.enable = true;
+        };
       };
     };
   };
