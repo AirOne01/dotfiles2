4c3200b30ba890d71e0150239eb59c3420da5441
AirOne01 <21955960+AirOne01@users.noreply.github.com>
2024-07-22 11:28:24 +0200

    fix(orion/git): patch git identity

--- modules/git.nix
+++ modules/git.nix
@@ -8,5 +8,21 @@
 
     userName = "AirOne01";
     userEmail = "21955960+AirOne01@users.noreply.github.com";
+
+    aliases = {
+      c = "checkout";
+      cc = "commit";
+      l = "log --all --graph --oneline";
+      p = "pull";
+      pp = "push";
+    };
+  };
+
+  programs.gh = {
+    enable = true;
+    gitCredentialHelper = {
+      enable = true;
+      hosts = [ "https://github.com" "https://gist.github.com" ];
+    };
   };
 }
