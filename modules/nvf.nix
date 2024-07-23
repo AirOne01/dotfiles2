f880c09c6290a5d71c934ec6a411f9f064fb1aa0
AirOne01 <21955960+AirOne01@users.noreply.github.com>
2024-07-23 17:02:48 +0200

    feat(nvf): add multiple plugins
    Including:
    - TS, HTML, NIX languages support (and tree-sitter support)
    - `lualine` as the status line w/ custom config
    - `tre-sitter`
    - `telescope`
    - multiple visual goodies
      - cursor line
      - web icons
      - `fidget` notifications widget
      - undo/redo highlight
      - scrollbar (and smooth scrolling)
    
    See #2.

--- modules/nvf.nix
+++ modules/nvf.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+_: {
   programs.nvf = {
     enable = true;
 
@@ -12,18 +12,107 @@
         };
 
         languages = {
-          nix.enable = true;
-          html.enable = true;
-          ts.enable = true;
+          nix = {
+            enable = true;
+            treesitter.enable = true;
+          };
+          ts = {
+            enable = true;
+            treesitter.enable = true;
+          };
+          html = {
+            enable = true;
+            treesitter.enable = true;
+          };
         };
 
-	      #startPlugins = [
-	      #  pkgs.vimPlugins.telescope-nvim
-	      #];
+        statusline.lualine = {
+          enable = true;
 
-	      statusline.lualine = {
-	        enable = true;
-	      };
+          theme = "nightfly";
+
+          activeSection = {
+            a = [
+              ''
+                {
+                  "mode",
+                  icons_enabled = true,
+                  separator = {
+                    right = '',
+                  },
+                }
+              ''
+            ];
+            b = [
+              ''
+                {
+                  "filetype",
+                  colored = true,
+                  icon_only = true,
+                  icon = { align = 'left' }
+                }
+              ''
+              ''
+                {
+                  "filename",
+                  symbols = { modified = ' ', readonly = ' ' },
+                }
+              ''
+            ];
+            c = [
+              ''
+                {
+                  "diff",
+                  colored = false,
+                  diff_color = {
+                    -- Same color values as the general color option can be used here
+                    added    = 'DiffAdd',    -- Changes the diff's added color
+                    modified = 'DiffChange', -- Changes the diff's modified color
+                    removed  = 'DiffDelete', -- Changes the diff's removed color
+                  },
+                  symbols = { added = ' ', modified = ' ', removed = ' ' } -- Change the diff symbol
+                }
+              ''
+            ];
+            z = [
+              ''
+                {
+                  "",
+                  draw_empty = true,
+                  separator = { left = '' }
+                }
+              ''
+              ''
+                { "progress" }
+              ''
+              ''
+                { "location" }
+              ''
+              ''
+                {
+                  "fileformat",
+                  color = { fg = 'black' },
+                  symbols = {
+                    unix = '', -- e712
+                    dos = '',  -- e70f
+                    mac = '',  -- e711
+                  }
+                }
+              ''
+            ];
+          };
+        };
+
+        treesitter = {
+          enable = true;
+
+          # (x?)html tag auto rename
+          autotagHtml = true;
+
+          context = {
+            enable = true;
+          };
+        };
 
         telescope = {
           enable = true;
@@ -32,6 +121,45 @@
         theme = {
           name = "tokyonight";
           style = "darker";
+        };
+
+        visuals = {
+          enable = true;
+
+          # hmm...
+          cellularAutomaton = {
+            #enable = true;
+            # default bind is "<leader>fml"
+            # ...for "fuck my life"
+          };
+          # highlight cursor
+          cursorline.enable = true;
+          # icons
+          nvimWebDevicons.enable = true;
+          # notification widget
+          fidget-nvim = {
+            enable = true;
+
+            setupOpts = {
+              # TODO: setup options here
+
+              # nvim-tree
+              integration.nvim-tree.enable = true;
+              # widget
+              notification.window.winblend = 75; # opacity%
+            };
+          };
+          # highlight undo/redo
+          highlight-undo.enable = true;
+          # indent blankline
+          indentBlankline = {
+            enable = true;
+
+            setupOpts.scope.enabled = true;
+          };
+          # scollbar
+          scrollBar.enable = true;
+          smoothScroll.enable = true;
         };
       };
     };
