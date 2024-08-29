141ea708269b9656c3f105fb5fd2ef59154aaa4f
Erwann Lagouche (AirOne01) <21955960+AirOne01@users.noreply.github.com>
2024-08-29 02:10:08 +0200

    fix(caddy): hacky solution before pr merge

--- constellations/aquarius/services/caddy.nix
+++ constellations/aquarius/services/caddy.nix
@@ -1,29 +1,33 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   services.caddy = {
     enable = true;
 
     package = pkgs.caddy-custom.override {
-      # externalPlugins = [
-      #   {
-      #     name = "cloudflare";
-      #     repo = "github.com/caddy-dns/cloudflare";
-      #     version = "89f16b99c18ef49c8bb470a82f895bce01cbaece";
-      #   }
-      #   {
-      #     name = "cache-handler";
-      #     repo = "github.com/caddyserver/cache-handler";
-      #     version = "v0.13.0";
-      #   }
-      # ];
-      # ++ (
-      #   # Caddy Layer4 modules
-      #   lib.lists.map (name: {
-      #     inherit name;
-      #     repo = "github.com/mholt/caddy-l4";
-      #     version = "3d22d6da412883875f573ee4ecca3dbb3fdf0fd0";
-      #   }) ["layer4" "modules/l4proxy" "modules/l4tls" "modules/l4proxyprotocol"]
-      # );
-      vendorHash = "sha256-bzl86zOScHH2f6m0w8H46snIWkyjta2Jl1Ht+/xp2Bs=";
+      caddyModules =
+        [
+          {
+            name = "cloudflare";
+            repo = "github.com/caddy-dns/cloudflare";
+            version = "89f16b99c18ef49c8bb470a82f895bce01cbaece";
+          }
+          {
+            name = "cache-handler";
+            repo = "github.com/caddyserver/cache-handler";
+            version = "v0.13.0";
+          }
+        ]
+        ++ (
+          # Caddy Layer4 modules
+          lib.lists.map (name: {
+            inherit name;
+            repo = "github.com/mholt/caddy-l4";
+            version = "3d22d6da412883875f573ee4ecca3dbb3fdf0fd0";
+          }) ["layer4" "modules/l4proxy" "modules/l4tls" "modules/l4proxyprotocol"]
+        );
     };
 
     virtualHosts."localhost".extraConfig = ''
--- overlays/caddy-custom/default.nix
+++ overlays/caddy-custom/default.nix
@@ -3,5 +3,6 @@ _: prev: {
   #
   # Source: https://github.com/NixOS/nixpkgs/pull/259275
   # Also see: https://github.com/NixOS/nixpkgs/issues/14671#issuecomment-2312320822
-  caddy-custom = prev.callPackage ./complex-stuff.nix {};
+  # The hacky solution: https://github.com/NixOS/nixpkgs/issues/14671#issuecomment-2316411251
+  caddy-custom = prev.callPackage ./package.nix {};
 }
